5e0517cfaf7afc4d74b21fd5a3c6f4962e72b236
Enrique Priego <enriquejose.priego@bosonit.com>
2023-02-27 11:17:31 +0100

    Añadido archivo YML

--- block5-properties/src/main/java/com/bosonit/formacion/block5properties/Block5PropertiesApplication.java
+++ block5-properties/src/main/java/com/bosonit/formacion/block5properties/Block5PropertiesApplication.java
@@ -8,12 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Block5PropertiesApplication {
 
+	//Propiedades con application.properties
 	@Value("${greeting}")
 	private String greeting;
 	@Value("${my.number}")
 	private int number;
 	@Value("${new.property:new.property no tiene valor}")
 	private String nueva;
+
+	//Propiedades con YAML
+	@Value("${prueba}")
+	private String prueba;
+	@Value("${numero.nuevo}")
+	private int numNuevo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Block5PropertiesApplication.class, args);
@@ -24,5 +31,7 @@ public class Block5PropertiesApplication {
 		System.out.println("Valor de greeting: " + greeting);
 		System.out.println("Valor de my.number: " + number);
 		System.out.println("Valor de new.property: " + nueva);
+		System.out.println("Valor de prueba: " + prueba);
+		System.out.println("Valor de numero nuevo: " + numNuevo);
 	}
 }

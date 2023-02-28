1d31af5faf710b5ec96514e19b3ce21bc21578de
Enrique Priego <enriquejose.priego@bosonit.com>
2023-02-28 13:40:33 +0100

    Creando archivos properties

--- block5-profiles/src/main/java/com/bosonit/formacion/block5profiles/Block5ProfilesApplication.java
+++ block5-profiles/src/main/java/com/bosonit/formacion/block5profiles/Block5ProfilesApplication.java
@@ -1,13 +1,28 @@
 package com.bosonit.formacion.block5profiles;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class Block5ProfilesApplication {
+	@Autowired
+	Environment env;
+
+	@Value("${bd.url}")
+	private String url;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Block5ProfilesApplication.class, args);
 	}
 
+	@PostConstruct
+	private void postConstruct(){
+		System.out.println(env);
+		System.out.println("La url es: " + url);
+	}
 }

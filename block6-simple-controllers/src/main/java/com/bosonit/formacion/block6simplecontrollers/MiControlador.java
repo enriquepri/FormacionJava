5c43c11abdca0a57fd78a04a559d6421bca2a3cb
Enrique Priego <enriquejose.priego@bosonit.com>
2023-03-03 11:16:48 +0100

    Añadida Segunda Parte Ejercicio

--- block6-simple-controllers/src/main/java/com/bosonit/formacion/block6simplecontrollers/MiControlador.java
+++ block6-simple-controllers/src/main/java/com/bosonit/formacion/block6simplecontrollers/MiControlador.java
@@ -1,10 +1,7 @@
 package com.bosonit.formacion.block6simplecontrollers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -12,5 +9,11 @@ public class MiControlador {
     @GetMapping("/user/{nombre}")
     public ResponseEntity<String> user(@PathVariable String nombre){
         return ResponseEntity.ok("Hola " + nombre);
+    }
+
+    @PostMapping("/useradd")
+    @ResponseBody
+    public ResponseEntity<Persona> userAdd(@RequestBody Persona person){
+        return ResponseEntity.ok().body(new Persona(person.nombre, person.poblacion, person.edad+1));
     }
 }

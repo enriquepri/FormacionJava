fc14fdec606b7584d438bc1737edbd78494c3e22
Enrique Priego <enriquejose.priego@bosonit.com>
2023-03-06 14:01:25 +0100

    Subir Ejercicio 3 Bloque 6

---
+++ block6-path-variable-headers/src/main/java/com/bosonit/formacion/block6pathvariableheaders/Controller.java
@@ -0,0 +1,44 @@
+package com.bosonit.formacion.block6pathvariableheaders;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class Controller {
+
+    @PostMapping("/noEntiendo")
+    public Saludo noEntiendo(@RequestBody Saludo saludo){
+        return saludo;
+    }
+
+    @PostMapping("/postPersona")
+    @ResponseBody
+    public ResponseEntity<Person> postPersona(@RequestBody Person person){
+        Person persona = new Person(person.getEdad(), person.getNombre());
+        return ResponseEntity.ok(persona);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<String> user(@PathVariable int id){
+        return ResponseEntity.ok("El id recibido es: " + id);
+    }
+
+    @PutMapping("/post")
+    @ResponseBody
+    public ResponseEntity<List> post(@RequestParam (defaultValue = "1") int var1,
+                                        @RequestParam (defaultValue = "2") int var2){
+        HashMap<String, Integer> mapa = new HashMap<>();
+        mapa.put("var1", var1);
+        HashMap<String, Integer> mapa2 = new HashMap<>();
+        mapa2.put("var2", var2);
+        List<HashMap> lista = new ArrayList<>();
+        lista.add(mapa2);
+        lista.add(mapa);
+        return ResponseEntity.ok(lista);
+    }
+}
---
+++ block6-path-variable-headers/src/main/java/com/bosonit/formacion/block6pathvariableheaders/Saludo.java
@@ -0,0 +1,11 @@
+package com.bosonit.formacion.block6pathvariableheaders;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Saludo {
+    //private String ayuda;
+    private int num;
+}

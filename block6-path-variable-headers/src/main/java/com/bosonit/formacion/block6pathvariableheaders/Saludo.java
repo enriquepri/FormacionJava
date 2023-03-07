99f3dce0ffd226a5f10e22cb93c15c0f436662ed
Enrique Priego <enriquejose.priego@bosonit.com>
2023-03-07 09:08:14 +0100

    Ajuste clase auxiliar

--- block6-path-variable-headers/src/main/java/com/bosonit/formacion/block6pathvariableheaders/Saludo.java
+++ block6-path-variable-headers/src/main/java/com/bosonit/formacion/block6pathvariableheaders/Saludo.java
@@ -3,9 +3,13 @@ package com.bosonit.formacion.block6pathvariableheaders;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/*Esta clase al final no la use para el ejercicio,
+la mantengo para la duda de por que no funciona el endpoint
+si el JSON solo tiene 1 parametro
+ */
 @Data
 @AllArgsConstructor
 public class Saludo {
-    //private String ayuda;
+    //private String texto;
     private int num;
 }
